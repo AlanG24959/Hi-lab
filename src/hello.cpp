d73e66df2ef99d0b376552c6653e8302f8cb2e57
AlanG24959 <ga83679@mail.harpercollege.edu>
2025-09-18 19:45:20 -0500

    HI lab tests

--- src/hello.cpp
+++ src/hello.cpp
@@ -7,7 +7,7 @@ int compIgnore(const std::string& s1, const std::string& s2) {
      for (size_t i = 0; i < n; ++i) {
           char c1 = tolower(static_cast<unsigned char>(s1[i]));
           char c2 = tolower(static_cast<unsigned char>(s2[i]));
-          if (c1 != c2) return c1 - c2;
+          if (c1 != c2) return (c1 < c2) ? -1 : 1;
      }
 
      if (s1.size() == s2.size()) return 0;
